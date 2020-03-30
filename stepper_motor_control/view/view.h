da3564c1f605723765dec8ced81c8a4cd3dc3e2a
Maxim <KrMax-steam@yandex.ru>
2020-03-30 23:45:04 +0300

    [view] Rename w to _main_window

--- stepper_motor_control/view/view.h
+++ stepper_motor_control/view/view.h
@@ -22,15 +22,13 @@ class View
 
 public:
     View() {
-//        strint
-
-        QPushButton* start_button = w.findChild<QPushButton*>("StartPushButton");
-        QPushButton* close_button = w.findChild<QPushButton*>("ClosePushButton");
+        QPushButton* start_button = _main_window.findChild<QPushButton*>("StartPushButton");
+        QPushButton* close_button = _main_window.findChild<QPushButton*>("ClosePushButton");
         std::cout << close_button;
     }
 
     void show(){
-        w.show();
+        _main_window.show();
     }
 
     QPushButton& get_button(string& name);
@@ -39,12 +37,12 @@ public:
     QRadioButton& get_radio_button(string& name);
 
 private:
-    MainWindow w;
 
     std::array<QPushButton, 5> _button;
     std::array<QCheckBox, 5> _checkbox;
     std::array<QLineEdit, 5> _lineedit;
     std::array<QRadioButton, 5> _radio_button;
+    MainWindow _main_window;
 };
 
 
